ee7d4d25f72ffe07b74f85c7841aebb7e57b2620
DESKTOP-JBAK0DT\86187 <1347402353@qq.com>
2023-09-28 02:27:12 +0800

    更改createContext文件

--- src/views/page1/index.tsx
+++ src/views/page1/index.tsx
@@ -1,6 +1,19 @@
+import { createContext ,useState} from "react";
+import "./page1.scss"
+
 function CreateContextTest(){
+  const ThemeContext = createContext('light');
+  const [theme,setTheme] = useState('light')
+
+  function handleClick(){
+    const th = theme=='light'?'dark':'light';
+    setTheme(th)
+  }
   return <>
-    <h1>这是一个createContext测试页面</h1>
+  <ThemeContext.Provider  value={theme}>
+    <button onClick={handleClick}>{theme=='light'?'to dark':'to light'}</button>
+    <h1 className={"theme-"+theme}>这是一段creatContext测试页面</h1>
+  </ThemeContext.Provider>
   </>
 }
 
